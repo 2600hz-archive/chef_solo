31c8fb91eb9c8074bf64535ae174ece7d22aabc7
Steve Lum <steve.lum@gmail.com>
2012-09-21 16:03:21 -0700

    added startup script to /etc/rc.local

--- cookbooks/basics/recipes/startup.rb
+++ cookbooks/basics/recipes/startup.rb
@@ -2,16 +2,9 @@
 # Cookbook Name:: basics
 # Recipe:: startup
 #
-# Copyright 2011, 2600hz, Inc.
+# Copyright 2012, 2600hz, Inc.
 #
 #
-
-%w{ rabbitmq-server ecallmgr whapps bigcouch }.each do |service|
-  execute "remove #{service} from " do
-    command "chkconfig --del #{service}"
-    ignore_failure true
-  end
-end
 
 template "/etc/rc.d/rc.local" do
   source "rc.local.erb"
