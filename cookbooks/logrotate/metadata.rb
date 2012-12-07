f3e58ee8f1552d92b90dbd6e686470dbaff31efb
Steve Lum <steve.lum@gmail.com>
2012-12-07 11:45:26 -0800

    updated logrotate cookbook version to 0.10.0

--- cookbooks/logrotate/metadata.rb
+++ cookbooks/logrotate/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  "scott@likens.us"
 license           "Apache 2.0"
 description       "Installs logrotate"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version           "0.8.4"
+version           "0.10.0"
 
 recipe "logrotate", "Installs logrotate"
 
