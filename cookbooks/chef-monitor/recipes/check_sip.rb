0f2bef1a6ed25df59c2b8810ab63146009b6aba8
Steve Lum <steve.lum@gmail.com>
2013-02-11 15:38:04 -0800

    added check_sip script and recipe

---
+++ cookbooks/chef-monitor/recipes/check_sip.rb
@@ -0,0 +1,37 @@
+#
+# Cookbook Name:: chef-monitor
+# Recipe:: check_sip
+#
+# Copyright 2013, Stephen Lum
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "chef-monitor::default"
+
+cookbook_file "/usr/lib64/nagios/plugins/check_sip" do
+  source "plugins/check_sip"
+  mode 0755
+end
+
+yum_package "nagios-plugins-all"
+
+yum_package "perl-Time-HiRes"
+
+sensu_check "check_sip" do
+  command "PATH=$PATH:/usr/lib64/nagios/plugins:/usr/lib/nagios/plugins check_sip -u sip:1234@#{node['ipaddress']} -p 5060 -s"
+  handlers ["default"]
+  subscribers ["freeswitch"]
+  interval 30
+  additional(:notification => "FreeSWITCH is not running")
+end
