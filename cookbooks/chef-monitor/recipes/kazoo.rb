0affdf9174c7ea5e185a8193ed3b20e4ba5929f0
Steve Lum <steve.lum@gmail.com>
2013-02-12 12:50:21 -0800

    added kazoo recipe to chef-monitor. added checks for ecallmgr and whistle_appr proceeses

---
+++ cookbooks/chef-monitor/recipes/kazoo.rb
@@ -0,0 +1,37 @@
+#
+# Cookbook Name:: chef-monitor
+# Recipe:: kazoo
+#
+# Copyright 2013, Stephen Lum
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "chef-monitor::default"
+include_recipe "chef-monitor::rabbitmq"
+
+sensu_check "check_ecallmgr_process" do
+  command "check-procs.rb -p '-name ecallmgr'"
+  handlers ["default"]
+  subscribers ["kazoo"]
+  interval 30
+  additional(:notification => "eCallManager is not running")
+end
+
+sensu_check "check_whistle_apps_process" do
+  command "check-procs.rb -p '-name whistle_apps'"
+  handlers ["default"]
+  subscribers ["kazoo"]
+  interval 30
+  additional(:notification => "Whistle Apps is not running")
+end
