65b2a4faf90a630559ac8b00c4effdf0b261fd9c
Steve Lum <steve.lum@gmail.com>
2013-03-25 10:39:28 -0700

    remove old sensu

--- cookbooks/sensu/resources/mutator.rb
+++
@@ -1,8 +0,0 @@
-actions :create, :delete
-
-attribute :command, :kind_of => String, :required => true
-
-def initialize(*args)
-  super
-  @action = :create
-end
--- cookbooks/sensu/resources/snippet.rb
+++
@@ -1,8 +0,0 @@
-actions :create, :delete
-
-attribute :content, :kind_of => Hash, :required => true
-
-def initialize(*args)
-  super
-  @action = :create
-end
