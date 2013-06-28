443cfb53abf567580433666fd5fbfc76ab77cecb
x974 <xavier@2600hz.com>
2013-06-27 17:51:27 -0700

    Updated check-ecallmgr-fs.sh

--- cookbooks/chef-monitor/files/default/plugins/check-ecallmgr-fs.sh
+++ cookbooks/chef-monitor/files/default/plugins/check-ecallmgr-fs.sh
@@ -8,20 +8,16 @@ E_UNKNOWN="3"
 erlangListener=`/usr/bin/fs_cli -x 'erlang listeners' |cut -d ' ' -f3 |cut -d '@' -f2 |grep -v "^[[:blank:]]*$"`
 line=`/usr/bin/fs_cli -x 'erlang listeners' |cut -d ' ' -f3 |cut -d '@' -f2 |grep -v "^[[:blank:]]*$" |wc -l`
 
-if [ "$line" -gt "1" ];
-        then
-
-        echo "$line ecallmgr  are currently connected:" $erlangListener
-        exit ${E_SUCCESS}
-
-elif [ "$line" = "1" ];
-        then
-
-        echo "Just one ecallmgr connected: $erlangListener"
-        exit ${E_WARNING}
-
+if ! [[ "$erlangListener" == listeners ]]; then
+        if [ "$line" -gt "1" ]; then
+        	echo "$line ecallmgr  are currently connected:" $erlangListener
+        	exit ${E_SUCCESS}
+        elif [ "$line" = "1" ]; then
+		echo "Just one ecallmgr connected: $erlangListener"
+	        exit ${E_WARNING}
+	fi
 else
-        echo "0 ecallmgr are connected to this FreeSWITCH!"
+	echo "0 ecallmgr are connected to this FreeSWITCH!"
         exit ${E_CRITICAL}
-
 fi
+exit ${E_UNKNOWN}
