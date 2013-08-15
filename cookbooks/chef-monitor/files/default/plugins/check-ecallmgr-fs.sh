b950a8ab62e0c8ed86ade4903b31f0f27e20842a
Jeremy Ai <jeremy.ai@2600hz.com>
2013-08-15 15:01:31 -0700

    Update check-ecallmgr-fs.sh
    Update this check to work with v3.0

--- cookbooks/chef-monitor/files/default/plugins/check-ecallmgr-fs.sh
+++ cookbooks/chef-monitor/files/default/plugins/check-ecallmgr-fs.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
-
+ 
 E_SUCCESS="0"
 E_WARNING="1"
 E_CRITICAL="2"
 E_UNKNOWN="3"
-
-erlangListener=`/usr/bin/fs_cli -x 'erlang listeners' |cut -d ' ' -f3 |cut -d '@' -f2 |grep -v "^[[:blank:]]*$"`
-line=`/usr/bin/fs_cli -x 'erlang listeners' |cut -d ' ' -f3 |cut -d '@' -f2 |grep -v "^[[:blank:]]*$" |wc -l`
-
-if ! [[ "$erlangListener" == listeners ]]; then
-        if [ "$line" -gt "1" ]; then
-        	echo "$line ecallmgr  are currently connected:" $erlangListener
-        	exit ${E_SUCCESS}
-        elif [ "$line" = "1" ]; then
-		echo "Just one ecallmgr connected: $erlangListener"
-	        exit ${E_WARNING}
-	fi
+ 
+listenerCount=`/usr/bin/fs_cli -H 127.0.0.1 -x 'erlang nodes count'`
+listenerList=`/usr/bin/fs_cli -H 127.0.0.1 -x 'erlang nodes list'`
+ 
+if [ "$listenerCount" -gt "1" ]
+then
+  echo -e "$listenerCount ecallmgr are currently connected:\n$listenerList"
+  exit ${E_SUCCESS}
+elif [ "$listenerCount" = "1" ]
+then
+  echo -e "Just one ecallmgr connected:\n$listenerList"
+  exit ${E_WARNING}
 else
-	echo "0 ecallmgr are connected to this FreeSWITCH!"
-        exit ${E_CRITICAL}
+   echo "0 ecallmgr are connected to this FreeSWITCH!"
+   exit ${E_CRITICAL}
 fi
+ 
 exit ${E_UNKNOWN}
