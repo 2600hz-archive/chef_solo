2b2e626460d24416e844f20d26c86dae0cd31e95
Xav 2600hz <xavier@2600hz.com>
2013-08-20 19:34:45 +0200

    Modified the Kazoo_api role run list to be complete. Might consider removing RabbitMQ for more flexibility in the future.

--- roles/kazoo_api.rb
+++ roles/kazoo_api.rb
@@ -1,3 +1,3 @@
 name "kazoo_api"
 description "Role for monitoring Kazoo API"
-run_list "recipe[chef-monitor::kazoo_api]"
+run_list "recipe[chef-monitor::default]", "recipe[chef-monitor::base_checks]", "recipe[chef-monitor::kazoo_api]", "recipe[chef-monitor::rabbitmq]"
